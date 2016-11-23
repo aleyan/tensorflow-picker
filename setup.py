47f502a1f126dac57e0fb08ec45938e22b0280b9
Alexander Yankov <alex@Alexanders-MacBook-Air.local>
2016-11-22 22:48:01 -0500

    Initial revision

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tensorflow-picker',
+      version='0.9',
+      description='Rolls the right TensorFlow wheel for you.',
+      url='http://github.com/aleyan/tensorflow-picker',
+      author='Alexander Yankov',
+      author_email='yankov.alex@gmail.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
